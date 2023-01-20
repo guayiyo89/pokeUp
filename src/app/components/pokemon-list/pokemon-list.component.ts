0b6a2b4a6bb63190a16311e70adc733ad4af5973
guayiyo89 <guayiyo89@gmail.com>
2023-01-19 21:57:30 -0300

    inicio paginacion

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -12,7 +12,10 @@ export class PokemonListComponent implements OnInit {
   listPokemon: Base[] = [];
   load = false;
   searchPokemon = new FormControl('');
-  pageActual: Base [] = [];
+  pageActual: Base[] = [];
+  pageSize = 5;
+  lastPage = 1;
+  currentPage = 1;
 
   constructor(private pokeSvc: PokemonService) {}
 
@@ -23,17 +26,30 @@ export class PokemonListComponent implements OnInit {
   getPokemons() {
     this.pokeSvc.getPokemons1stGen().subscribe((resp) => {
       this.listPokemon = resp.pokemon_species;
+      this.lastPage = this.calcularPaginas(this.listPokemon, this.pageSize);
+      this.pageActual = this.listPokemon;
       this.load = true;
     });
   }
 
-  get showPokemon() {
-    return this.listPokemon.filter(
-      (specie) => specie.name.includes(this.searchPokemon.value)
+  showPokemon() {
+    this.pageActual = this.listPokemon.filter((specie) =>
+      specie.name.includes(this.searchPokemon.value)
     );
   }
 
   paginate(array: Base[], page_size: number, page_number: number) {
     return array.slice((page_number - 1) * page_size, page_number * page_size);
   }
+
+  calcularPaginas(array: Base[], page_size: number){
+    let totalPaginas;
+    if(array.length % page_size == 0){
+      totalPaginas = array.length / page_size
+    } else {
+      totalPaginas = Math.trunc(array.length / page_size) + 1;
+    }
+    return totalPaginas
+  }
+  
 }
